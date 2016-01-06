8c7e62da9a8b5f8ad704ff7ea67eb7c68e349780
ruanima <190127701@qq.com>
2016-01-06 20:24:39 +0800

    done proj1

--- 1.InstantMarkup/util.py
+++ 1.InstantMarkup/util.py
@@ -1,12 +1,15 @@
+#coding=utf-8
+
 def lines(file):
     for line in file: yield line
     yield '\n'
 
 def blocks(file):
     block = []
-    for line in file:
+    for line in lines(file):
         if line.strip():
             block.append(line)
         elif block:
             yield ''.join(block).strip()
             block = []
+
